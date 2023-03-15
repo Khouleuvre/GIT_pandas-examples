751630fcdb53cd9654251fa481212b6b124b8e32
Abhinav-grenoble <abhinav.shibu@gmail.com>
2023-03-15 09:43:30 +0100

    abhinav working for conflict

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -61,6 +61,6 @@ print('\n==================================================================\n')
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
 rangement = cloneDF(mergeRatings)
-rangement = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+rangement = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg()
     COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
 print('My info sorted: \n%s' % sortRatingsField[:15])
